01875a46725623488df02026f508dfd488feec67
Draius <41041066+draius@users.noreply.github.com>
2019-09-17 14:57:49 -0400

    Finish Assignment

---
+++ tree.py
@@ -0,0 +1,13 @@
+Main Method:
+
+
+  n = int(input("Input # of Branches : "))
+  
+  for x in range(n):
+  
+    print((" " * (n - x)),('#' * (2*x+1)))
+    
+  print(n*" ", "#")
+
+
+Call it with: main()
